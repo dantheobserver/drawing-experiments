748bb8743f0f55635b6093ceafe79ca37e630a1d
dantheobserver <daniel.alan.p@gmail.com>
2018-07-03 17:59:06 -0700

    * Randomize y placement, update with boxing warnings.

--- src/experiments/01_confetti.clj
+++ src/experiments/01_confetti.clj
@@ -4,42 +4,45 @@
             [fastmath.core :as m]
             [fastmath.random :as r]))
 
+(set! *warn-on-reflection* true)
+(set! *unchecked-math* :warn-on-boxed)
+
 (def width 800)
 (def height 600)
-(def canvas-size [width height])
-(def center-canvas (into [] (map #(/ % 2) canvas-size)))
 
+(def canvas-size [width height])
+(def center-canvas (into [] (map #(/ ^long % 2) canvas-size)))
 
 (defn rcolor [] (r/irand 255))
+(defn rrate [] (r/frand 10 30))
 
 (defn setup [_ _]
   {:droplets (take 1000 (repeatedly (fn []
                                      (let [size (r/irand 10 20)]
                                        {:size [size size]
                                         :color [(rcolor) (rcolor) (rcolor) (r/irand 120 255)]
-                                        :rate (r/frand 10 30)
-                                        :pos [(r/frand width) 0]}))))})
+                                        :rate (rrate)
+                                        :pos [(r/frand width) (r/frand height)]}))))})
 
 (defn move-droplet [droplet]
-  (let [{:keys [pos rate]} droplet 
-        [_ y] pos]
-    (if (< y height)
-      (update-in droplet [:pos 1] #(+ rate %))
+  (let [{:keys [pos ^long rate]} droplet
+        [_ ^long y] pos]
+    (if (< y ^long height)
+      (update-in droplet [:pos 1] #(+ rate ^long %))
       (-> droplet
-          (assoc :rate (r/frand 10 30))
+          (assoc :rate (rrate))
           (assoc-in [:pos 0] (r/frand width)) ;;new x
           (assoc-in [:pos 1] 0)))))
 
 (defn draw [canvas window frame {:keys [droplets] :as state}]
   (c/set-background canvas :black)
-  (doall
-   (for [droplet droplets]
+  (doseq [droplet droplets]
     (let [{:keys [size color pos]} droplet
-           [w h] size
-           [x y] pos]
-       (-> canvas
-           (c/set-color color)
-           (c/ellipse x y w h)))))
+          [w h] size
+          [x y] pos]
+      (-> canvas
+          (c/set-color color)
+          (c/ellipse x y w h))))
   (update state :droplets #(map move-droplet %)))
 
 (defn run []
@@ -50,5 +53,3 @@
         :fps 30
         :draw-fn draw
         :setup setup}))))
-
-(run)
