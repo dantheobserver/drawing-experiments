00afec79a12df139d40139d9908fc5819bf88af7
dantheobserver <daniel.alan.p@gmail.com>
2019-01-13 20:50:18 -0800

    Cleanup
    * Moved some useful utils in common.utils.
    * anon-proxy fix by converting to macro.

--- src/common/utils.clj
+++ src/common/utils.clj
@@ -1,12 +1,10 @@
 (ns common.utils
   (:require [fastmath.core :as m]))
 
-(defn proxy-fn
-  "Returns a function that proxies `draw-fn`"
+(defmacro anon-proxy
   [draw-fn]
-  (fn [& args]
-    (apply draw-fn args)))
-
+  `(fn [& ~'args]
+     (apply ~draw-fn ~'args)))
 
 (defn pi [factor] (* m/PI factor))
 
@@ -18,3 +16,27 @@
   `(->> (list ~@args)
         (interleave (map keyword '(~@args)))
         (apply hash-map)))
+
+(defn projected-point
+  "Given a point with `x`,`y`,`z` and
+  a vanishing point with ``"
+  [[x y z] [xv yv zv]]
+  (cond
+    (= z 0) [x y]
+    (= z zv) [xv yv zv]
+    :else (let [dist (m/dist x y xv yv)
+                dx (- xv x)
+                dy (- yv y)
+                pt-dist (* z (/ dist zv))
+                ratio (/ pt-dist dist)
+                offsets (mapv (partial * ratio) [dx dy])]
+            (mapv + [x y] offsets))))
+
+(comment
+  (let [draw-fn (fn [] 1)
+        aproxy (anon-proxy draw-fn)]
+    #_(macroexpand '(anon-proxy draw-fn))
+    (aproxy))
+  (let [test (fn [] (println "testing"))
+        a-proxy (anon-proxy proxy-fn)]
+    (a-proxy)))
--- src/experiments/04_projection.clj
+++ src/experiments/04_projection.clj
@@ -2,7 +2,7 @@
   (:require [clojure2d.core :as c]
             [fastmath.core :as m]
             [fastmath.random :as r]
-            [common.utils :as utils :use [pi-memoized :as pi]]
+            [common.utils :as utils]
             [fastmath.core :as math]))
 
 (def w 500)
@@ -13,30 +13,34 @@
       (c/set-color [255 0 0])
       (c/ellipse xv yv 5 5)))
 
-(defn- projected-point
-  [[x y z :as point]
-   [xv yv zv :as vanishing-point]]
-  (let [dist (m/dist x y xv yv)
-        dx (- xv x)
-        dy (- yv y)
-        pt-dist (* z (/ dist zv))
-        ratio (/ pt-dist dist)
-        offsets (mapv (partial * ratio) [dx dy])]
-    (mapv + [x y] offsets)))
-
 (defn draw-points
-  [canvas z-points [x y] vp]
-  (doseq [z z-points]
-    (let [ [x' y'] (projected-point [x y z] vp)]
+  [canvas points vp]
+  (doseq [point points]
+    (let [[x' y'] (utils/projected-point point vp)]
       (-> canvas
           (c/set-color [0 0 255])
-          (c/ellipse x' y' 4 4)))))
+          (c/ellipse x' y' 4 4))))
+  canvas)
+
+(defn draw-polygon [canvas vp]
+  (let [shape '([0 0 0] [0 300 0] [100 50 100] [400 0 44])
+        polygon (map #(utils/projected-point % vp) shape)]
+   (-> canvas
+        (c/set-color [255 0 0])
+        (c/filled-with-stroke
+         [255 0 0] [0 0 0]
+         c/path polygon true)
+        #_(c/path polygon true false))))
 
 (defn draw [canvas window frame {:keys [vp points] :as state}]
+
   (-> canvas
       (c/set-background [255 255 255])
       (draw-vanishing-point vp)
-      (draw-points points (:point (c/get-state window)) vp))
+      (draw-polygon vp)
+      ;; (draw-points points vp)
+      #_(draw-points points vp)
+      #_(draw-points points (:point (c/get-state window)) vp))
   state)
 
 (defn run []
@@ -48,18 +52,20 @@
         window-name "point projection"
         window (c/show-window {:canvas (c/canvas w h)
                                :window-name window-name
-                               :draw-fn (utils/proxy-fn draw)
+                               :draw-fn (utils/anon-proxy draw)
                                :draw-state (utils/map-keyed vp points)
                                :state {:point [0 0]}
                                :fps 30})]
 
     (.setAlwaysOnTop (:frame window) true)
 
-    (defmethod c/mouse-event [window-name :mouse-moved] [e state]
-      (let [mouse-pos ((juxt c/mouse-x c/mouse-y) e)]
-        (c/set-state! window (assoc state :point mouse-pos))
-        )))
-  )
-
+    ;; Update state with mouse position.
+    #_(defmethod c/mouse-event [window-name :mouse-moved] [e state]
+        (let [mouse-pos ((juxt c/mouse-x c/mouse-y) e)]
+          (c/set-state! window (assoc state :point mouse-pos))))))
 
 (run)
+
+#_(let [shape '([0 0 0] [0 50 30] [100 50 5] [100 0 0])
+        projected (map #(utils/projected-point % [255 255 40]) shape)]
+    projected)
